25f2c462a79a1125a86e9b56a56e4753311ab19e
HangNT169 <hangnt169@fpt.edu.vn>
2026-01-09 16:54:56 +0700

    Buổi 3: Spring validation

--- src/main/java/com/poly/demo/Sof3022Sd20305Bl1Sp26Application.java
+++ src/main/java/com/poly/demo/Sof3022Sd20305Bl1Sp26Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sof3022Sd20305Bl1Sp26Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Sof3022Sd20305Bl1Sp26Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Sof3022Sd20305Bl1Sp26Application.class, args);
+    }
 
 }
--- src/test/java/com/poly/demo/Sof3022Sd20305Bl1Sp26ApplicationTests.java
+++ src/test/java/com/poly/demo/Sof3022Sd20305Bl1Sp26ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Sof3022Sd20305Bl1Sp26ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
